800d7848a8c70360f058e01b91361b7a8bf0aef0
Yanik Ammann <confused@ant.lgbt>
2021-10-30 13:10:44 +0200

    increased grid space

--- snake.js
+++ snake.js
@@ -20,24 +20,20 @@ var upDirection = false;
 var downDirection = false;
 var inGame = true;    
 
-const DOT_SIZE = 10;
-const ALL_DOTS = 900;
-const MAX_RAND = 29;
-const DELAY = 140;
-const C_HEIGHT = 300;
-const C_WIDTH = 300;    
+const DOT_SIZE = 20;
+const DELAY = 200;
+const C_HEIGHT = 600;
+const C_WIDTH = 600;    
 
 const LEFT_KEY = 37;
 const RIGHT_KEY = 39;
 const UP_KEY = 38;
 const DOWN_KEY = 40;
 
-var x = new Array(ALL_DOTS);
-var y = new Array(ALL_DOTS);   
-
+var x = new Array(C_WIDTH);
+var y = new Array(C_HEIGHT);   
 
 function init() {
-    
     canvas = document.getElementById('myCanvas');
     ctx = canvas.getContext('2d');
 
@@ -48,7 +44,6 @@ function init() {
 }    
 
 function loadImages() {
-    
     head = new Image();
     head.src = 'head.png';    
     
@@ -60,19 +55,16 @@ function loadImages() {
 }
 
 function createSnake() {
-
     dots = 3;
-
+    // initial position
     for (var z = 0; z < dots; z++) {
-        x[z] = 50 - z * 10;
-        y[z] = 50;
+        x[z] = (3 * DOT_SIZE) - z * DOT_SIZE;
+        y[z] = (3 * DOT_SIZE) ;
     }
 }
 
 function checkApple() {
-
     if ((x[0] == apple_x) && (y[0] == apple_y)) {
-
         dots++;
         locateApple();
     }
@@ -92,8 +84,10 @@ function doDrawing() {
                 ctx.drawImage(head, x[z], y[z]);
             } else {
                 ctx.drawImage(ball, x[z], y[z]);
+                console.log("ball", x, y)
             }
-        }    
+        }   
+
     } else {
 
         gameOver();
@@ -101,87 +95,57 @@ function doDrawing() {
 }
 
 function gameOver() {
-    
     ctx.fillStyle = 'white';
     ctx.textBaseline = 'middle'; 
     ctx.textAlign = 'center'; 
-    ctx.font = 'normal bold 18px serif';
+    ctx.font = 'normal bold 24px serif';
     
     ctx.fillText('Game over', C_WIDTH/2, C_HEIGHT/2);
 }
 
 function checkApple() {
-
     if ((x[0] == apple_x) && (y[0] == apple_y)) {
-
         dots++;
         locateApple();
     }
 }
 
 function move() {
-
     for (var z = dots; z > 0; z--) {
         x[z] = x[(z - 1)];
         y[z] = y[(z - 1)];
     }
 
-    if (leftDirection) {
+    if (leftDirection)
         x[0] -= DOT_SIZE;
-    }
-
-    if (rightDirection) {
+    if (rightDirection)
         x[0] += DOT_SIZE;
-    }
-
-    if (upDirection) {
+    if (upDirection)
         y[0] -= DOT_SIZE;
-    }
-
-    if (downDirection) {
+    if (downDirection)
         y[0] += DOT_SIZE;
-    }
 }    
 
 function checkCollision() {
-
-    for (var z = dots; z > 0; z--) {
-
-        if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
+    for (var z = dots; z > 0; z--)
+        if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z]))
             inGame = false;
-        }
-    }
 
-    if (y[0] >= C_HEIGHT) {
+    if (y[0] >= C_HEIGHT || y[0] < 0 || x[0] >= C_WIDTH  || x[0] < 0)
         inGame = false;
-    }
-
-    if (y[0] < 0) {
-       inGame = false;
-    }
-
-    if (x[0] >= C_WIDTH) {
-      inGame = false;
-    }
-
-    if (x[0] < 0) {
-      inGame = false;
-    }
 }
 
 function locateApple() {
-
-    var r = Math.floor(Math.random() * MAX_RAND);
+    var r = Math.floor(Math.random() * C_WIDTH / DOT_SIZE);
     apple_x = r * DOT_SIZE;
+    console.log("not snek", r * DOT_SIZE)
 
-    r = Math.floor(Math.random() * MAX_RAND);
+    r = Math.floor(Math.random() * C_HEIGHT / DOT_SIZE);
     apple_y = r * DOT_SIZE;
 }    
 
 function gameCycle() {
-    
     if (inGame) {
-
         checkApple();
         checkCollision();
         move();
@@ -191,32 +155,27 @@ function gameCycle() {
 }
 
 onkeydown = function(e) {
-    
-    var key = e.keyCode;
+    let key = e.keyCode;
     
     if ((key == LEFT_KEY) && (!rightDirection)) {
-        
         leftDirection = true;
         upDirection = false;
         downDirection = false;
     }
 
     if ((key == RIGHT_KEY) && (!leftDirection)) {
-        
         rightDirection = true;
         upDirection = false;
         downDirection = false;
     }
 
     if ((key == UP_KEY) && (!downDirection)) {
-        
         upDirection = true;
         rightDirection = false;
         leftDirection = false;
     }
 
     if ((key == DOWN_KEY) && (!upDirection)) {
-        
         downDirection = true;
         rightDirection = false;
         leftDirection = false;
